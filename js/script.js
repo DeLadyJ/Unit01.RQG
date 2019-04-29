03f9895a1239391239675eee3aada60257285491
Ja_Lew <dladyj@gmail.com>
2019-04-28 20:35:38 -0400

    Removed extra space

--- js/script.js
+++ js/script.js
@@ -77,7 +77,7 @@ function printQuote() {
   var html = '<p class = "quote">' + randomQuote.quote + '</p>' +
   '<p class =  "source">' + randomQuote.source;
 if (randomQuote.citation){
-html += '<span class = "citation" >' + randomQuote.citation + '</span>';
+html += '<span class = "citation">' + randomQuote.citation + '</span>';
 } 
 if (randomQuote.year){
 html += '<span class = "year"> ' + randomQuote.year + '</span>';
